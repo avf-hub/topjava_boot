7ae178c0934b84ffabebc30fcb63d039ec6b26f2
Andrey Falkovskiy <avf-91@yandex.ru>
2023-01-17 23:03:38 +0300

    fix patch: 12_3_update_fix.patch

--- src/main/java/ru/javaops/topjava/web/GlobalExceptionHandler.java
+++ src/main/java/ru/javaops/topjava/web/GlobalExceptionHandler.java
@@ -5,6 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.*;
+import org.springframework.lang.NonNull;
 import org.springframework.validation.FieldError;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -39,21 +40,32 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
         return handleExceptionInternal(ex, body, headers, HttpStatus.UNPROCESSABLE_ENTITY, request);
     }
 
-    @ExceptionHandler(DataConflictException.class)
-    public ResponseEntity<?> dataConflictException(DataConflictException ex, WebRequest request) {
-        log.error("DataConflictException: {}", ex.getMessage());
-        return createProblemDetailExceptionResponse(ex, HttpStatus.CONFLICT, request);
-    }
-
+    //   https://howtodoinjava.com/spring-mvc/spring-problemdetail-errorresponse/#5-adding-problemdetail-to-custom-exceptions
     @ExceptionHandler(AppException.class)
-    public ResponseEntity<?> appException(AppException ex, WebRequest request) {
+    public ProblemDetail appException(AppException ex, WebRequest request) {
         log.error("ApplicationException: {}", ex.getMessage());
-        return createProblemDetailExceptionResponse(ex, ex.getStatusCode(), request);
+        return createProblemDetail(ex, ex.getStatusCode(), request);
     }
 
-    private ResponseEntity<?> createProblemDetailExceptionResponse(Exception ex, HttpStatusCode statusCode, WebRequest request) {
-        ProblemDetail body = createProblemDetail(ex, statusCode, ex.getMessage(), null, null, request);
-        return handleExceptionInternal(ex, body, new HttpHeaders(), statusCode, request);
+    @ExceptionHandler(DataConflictException.class)
+    public ProblemDetail dataConflictException(DataConflictException ex, WebRequest request) {
+        log.error("DataConflictException: {}", ex.getMessage());
+        return createProblemDetail(ex, HttpStatus.CONFLICT, request);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ProblemDetail exception(Exception ex, WebRequest request) {
+        String msg = ex.getClass().getName();
+        log.error("Exception: " + msg, ex);
+        return createProblemDetail(ex, HttpStatus.INTERNAL_SERVER_ERROR, msg, request);
+    }
+
+    private ProblemDetail createProblemDetail(Exception ex, HttpStatusCode statusCode, WebRequest request) {
+        return createProblemDetail(ex, statusCode, ex.getMessage(), request);
+    }
+
+    private ProblemDetail createProblemDetail(Exception ex, HttpStatusCode statusCode, @NonNull String msg, WebRequest request) {
+        return createProblemDetail(ex, statusCode, msg, null, null, request);
     }
 
     private String getErrorMessage(ObjectError error) {
